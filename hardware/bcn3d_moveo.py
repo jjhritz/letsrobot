725b11e400e7fbf69bad3c2784559e885cefcc57
John J. Hritz <john-j-hritz@sbcglobal.net>
2018-12-07 12:03:57 -0800

    Completed implementation of command parser.  Passes unit tests. Still requires integration testing to ensure the G-code is properly formatted and will be recognized by the Marlin firmware.

--- hardware/bcn3d_moveo.py
+++ hardware/bcn3d_moveo.py
@@ -16,19 +16,30 @@ Date: 2018/12/5
 
 import hardware.serial_board as serial_board
 
+if __name__ == '__main__':
+    import sys
+
+    try:
+        from configparser import ConfigParser
+        robot_config = ConfigParser()
+    except ImportError:
+        print("Missing configparser module (python -m pip install configparser)")
+        sys.exit()
+
+
 module = None
 
 # The string names of the joints driven by stepper motors
 steppers = ('base', 'shoulder', 'elbow', 'wrist elevation', 'wrist rotation')
 # The string names of the joints driven by servos
-servos = ['gripper']
+servos = tuple(['gripper'])
 
 arm_axes = {}
 arm_constraints = {}
 arm_position = {steppers[0]: 0, steppers[1]: 0, steppers[2]: 0, steppers[3]: 0, steppers[4]: 0, servos[0]: 0}
 
 
-def bind_axes(robot_config):
+def bind_axes(robot_config: ConfigParser):
     """Defines which G-Code axis controls which joint on the arm.
 
     :param robot_config: The ConfigParser object representation of letsrobot.conf
@@ -43,7 +54,7 @@ def bind_axes(robot_config):
     arm_axes[servos[0]] = robot_config.get('bcn3d_moveo', 'gripper')
 
 
-def min_key(joint):
+def min_key(joint: str) -> str:
     """Constructs and returns a string in the form "<joint> min"
 
     :param joint: The name of the joint to be referenced
@@ -53,7 +64,7 @@ def min_key(joint):
     return joint + ' min'
 
 
-def max_key(joint):
+def max_key(joint: str) -> str:
     """Constructs and returns a string in the form "<joint> max"
 
     :param joint: The name of the joint to be referenced
@@ -63,7 +74,7 @@ def max_key(joint):
     return joint + ' max'
 
 
-def set_constraints(robot_config):
+def set_constraints(robot_config: ConfigParser):
     """Sets the movement constraints on the arm to prevent damage to the arm or its environment.
 
     Sets the minimum and maximum coordinate positions on each axis so they can be checked whenever the arm is moved.
@@ -85,7 +96,7 @@ def set_constraints(robot_config):
     arm_constraints[max_key(servos[0])] = robot_config.getint('bcn3d_moveo', 'gripper_max')
 
 
-def set_start_position(robot_config):
+def set_start_position(robot_config: ConfigParser):
     """Sets the position for each joint when the robot starts up.
 
     :param robot_config: The ConfigParser object representation of letsrobot.conf
@@ -101,7 +112,7 @@ def set_start_position(robot_config):
     arm_position[servos[0]] = robot_config.getint('bcn3d_moveo', 'gripper_s')
 
 
-def check_constraints(joint, pos):
+def check_constraints(joint: str, pos: int) -> int:
     """Checks if a movement position exceeds a joint's constraints.
     If the position does not exceed the joint's constraints, the position is returned.
     It the position DOES exceed the joint's constraints, the function returns the constraint that was exceeded.
@@ -127,23 +138,23 @@ def check_constraints(joint, pos):
         return pos
 
 
-def move_arm_to(base_pos=None,
-                shoul_pos=None,
-                elbow_pos=None,
-                elev_pos=None,
-                rot_pos=None,
-                grip_pos=None):
+def move_arm_to(base_pos: int = None,
+                shoul_pos: int = None,
+                elbow_pos: int = None,
+                elev_pos: int = None,
+                rot_pos: int = None,
+                grip_pos: int = None) -> list:
 
     """Orders a movement of the arm to an absolute position.
     Checks the passed positions against the joint constraints, constructs the G-Code commands based on those checks,
     and forwards the commands to the serial port.
 
-    :param base_pos: The new coordinate of the base.  Default: None; uses the current position.
-    :param shoul_pos: The new coordinate of the shoulder.  Default: None; uses the current position.
-    :param elbow_pos: The new coordinate of the elbow.  Default: None; uses the current position.
-    :param elev_pos: The new coordinate of the wrist elevation.  None; uses the current position.
-    :param rot_pos: The new coordinate of the rotation servo.  None; uses the current position.
-    :param grip_pos: The new position of the gripper servo.  None; uses the current position.
+    :param base_pos: The new coordinate of the base.  Default: None; will use current position.
+    :param shoul_pos: The new coordinate of the shoulder.  Default: None; will use current position.
+    :param elbow_pos: The new coordinate of the elbow.  Default: None; will use current position.
+    :param elev_pos: The new coordinate of the wrist elevation.  Default: None; will use current position.
+    :param rot_pos: The new coordinate of the rotation servo.  Default: None; will use current position.
+    :param grip_pos: The new position of the gripper servo.  Default: None; will use current position.
     :return: The new arm position.
     """
 
@@ -193,9 +204,10 @@ def move_arm_to(base_pos=None,
     return arm_position
 
 
-def move_arm_by(base_inc=0, shoul_inc=0, elbow_inc=0, elev_inc=0, rot_inc=0, grip_inc=0):
+def move_arm_by(base_inc=0, shoul_inc=0, elbow_inc=0, elev_inc=0, rot_inc=0, grip_inc=0) -> list:
     """Orders a movement of the arm to a new position relative to its current one.
-    Essentially just a wrapper for move_arm_to that calculates the new position based on the increment.
+    Essentially just a wrapper for move_arm_to()
+    that calculates the new position based on the increment.
 
     :param base_inc: Movement increment of the base.
     :param shoul_inc: Movement increment of the shoulder.
@@ -216,7 +228,7 @@ def move_arm_by(base_inc=0, shoul_inc=0, elbow_inc=0, elev_inc=0, rot_inc=0, gri
     return move_arm_to()
 
 
-def parse_command(command):
+def parse_command(command: str) -> list:
     """Splits the command into a list of tuples in the form (joint, inc|abs, distance).
     Where joint is the name of the joint (corresponds to an item in steppers or servos),
     inc|abs is either the string "inc" or "abs" and signals whether relative or absolute movement is to be used,
@@ -231,12 +243,40 @@ def parse_command(command):
     for subgroup in split_list:
         command_list.append(tuple(subgroup.split(',')))
     if __name__ == "__main__":
-        print(command_list)
+        print("Parsed command: ", command_list)
 
     return command_list
 
 
-def setup(robot_config):
+def set_joint_position(joint: str, move_type: str, coordinate: int):
+    """ Sets the position of a joint for next movement.
+    Will check constraints for movement before updating position.  Should be used in place of move_arm_to when building
+    synchronous moves from a larger queue.
+
+    :param joint: The joint to be moved.
+    :param move_type: 'inc' for incremental movement or 'abs' for absolute movement.
+    :param coordinate: The G-Code coordinate the joint will move to
+    :return: None
+    """
+    global arm_position
+    try:
+        if move_type == "abs":
+            arm_position[joint] = check_constraints(joint, coordinate)
+        elif move_type == "inc":
+            arm_position[joint] = check_constraints(joint, arm_position[joint] + coordinate)
+        else:
+            if __name__ == "__main__":
+                print("Malformed command: ", move_type, " is not a valid move type. ", (joint, move_type, coordinate))
+
+    except KeyError:
+        if __name__ == "__main__":
+            print("Malformed command: ", joint, " is not a valid joint. ", (joint, move_type, coordinate))
+    except TypeError:
+        if __name__ == "__main__":
+            print("Malformed command: ", str(coordinate), " is not an integer. ", (joint, move_type, coordinate))
+
+
+def setup(robot_config: ConfigParser):
     """Initialize options for the arm.
 
     :param robot_config: The ConfigParser object representation of letsrobot.conf
@@ -261,29 +301,30 @@ def move(args):
     This function is intended to take simple string commands from the server and convert them into the less
     human-readable G-code the MOVEO expects.
 
-    :param args: The command string received from the server
+    :param args: The command dictionary received from the server
     :return: None
     """
     # Your custom command interpreter code goes here
-    parse_command(args['command'])
-    #
+    command_list = parse_command(args['command'])
+
+    for command in command_list:
+        try:
+            set_joint_position(command[0], command[1], int(command[2]))
+        except IndexError:
+            print("Malformed Command: ", command, " only has ", len(command), " parts, not 3. ",
+                  "Should be of form (joint, move_type, coordinate).")
+        except TypeError or ValueError:
+            print("Malformed Command: ", command, " is not of form (str, str, int).")
+
+    move_arm_to()
 
 
 if __name__ == '__main__':
-    import sys
-
-    try:
-        from configparser import ConfigParser
-        robot_config = ConfigParser()
-    except ImportError:
-        print("Missing configparser module (python -m pip install configparser)")
-        sys.exit()
-
     try:
         robot_config.readfp(open('letsrobot.conf'))
     except IOError:
-        print(
-            "unable to read letsrobot.conf, please check that you have copied letsrobot.sample.conf to letsrobot.conf and modified it appropriately.")
+        print("unable to read letsrobot.conf, please check that you have copied letsrobot.sample.conf to ",
+              "letsrobot.conf and modified it appropriately.")
         sys.exit()
     except:
         print("Error in letsrobot.conf:", sys.exc_info()[0])
@@ -296,6 +337,55 @@ if __name__ == '__main__':
     print(arm_constraints)
     print(arm_position)
 
+    mode = input("Manual mode? Y/N:")
+    if mode.upper() == "Y":
+        print("Manual mode")
+    else:
+        import random
+        print("Auto mode")
+        random.seed()
+
+        def random_command() -> str:
+            """Constructs a random movement command for testing purposes.
+            Command will contain between 1 and 6 segments, such that a single joint will be commanded once
+            or not at all.  Some coordinates will be out of bounds.
+
+            :return: A properly formatted command string that can be parsed and interpreted.
+            """
+
+            num_parts = random.randint(1, 6)
+            used_joints = random.sample(list(steppers + servos), num_parts)
+            command = ""
+            for joint in used_joints:
+                move_type = random.choice(["inc", "abs"])
+                coordinate = random.randint(-200, 200)
+                command += joint + ',' + move_type + ',' + str(coordinate) + ';'
+
+            # Strip trailing semicolon
+            command = command.rstrip(';')
+
+            return command
+
     while True:
-        command = input('Enter command: ')
-        parse_command(command)
+        print()
+        print("Starting position:", arm_position)
+        if mode.upper() == "Y":
+            command = input('Enter command: ')
+        else:
+            command = random_command()
+            print(command)
+
+        command_list = parse_command(command)
+
+        for command in command_list:
+            try:
+                set_joint_position(command[0], command[1], int(command[2]))
+            except IndexError:
+                print("Malformed Command: ", command, " only has ", len(command), " parts, not 3. ",
+                      "Should be of form (joint, move_type, coordinate).")
+            except TypeError or ValueError:
+                print("Malformed Command: ", command, " is not of form (str, str, int).")
+
+        print(arm_position)
+        move_arm_to()
+        input("Press enter to continue.")
